5f23c0f3967f85fadd72f4c7153ee19507a19ef4
shouston3 <shouston3@me.com>
2016-09-15 21:17:44 +0100

    passing down all the data to the list iterator from the parent to keep it dumb

--- examples/static-list/list-server.js
+++ examples/static-list/list-server.js
@@ -1,9 +1,7 @@
-var riot  = require('riot');
+var riot = require('riot');
 // by requiring the itterator tag here it becomes available
 // as a tag inside list.tag when rendered by riot below
 require('./list-iterator.tag');
 var list = require('./list.tag');
-var html  = riot.render(list, { name: 'My List'});
+var html = riot.render(list, { name: 'My List' });
 console.log(html);
-
-module.exports = list;
