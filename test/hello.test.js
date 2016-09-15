abed380235ae70ba1e954f86198cc28d364ae2aa
shouston3 <shouston3@me.com>
2016-09-15 21:58:43 +0100

    changing file paths so the tests pass

--- test/hello.test.js
+++ test/hello.test.js
@@ -6,7 +6,7 @@ var file    = dir + __filename.replace(__dirname, '') + " -> ";
 var riot  = require('riot');
 
 test(file + "Hello World test", function(t) {
-  var hello = require('../examples/hello.tag'); // sync
+  var hello = require('../examples/hello-world/hello.tag'); // sync
   var html  = riot.render(hello, { name: "Ana" });
   t.ok(html.match(/Hello Ana/), "Hello renders to: "+html)
   t.end()
